d5c6c1d40142c8c3f830643af30c3a4170fd7f59
GuilhermeBeltrao <guilhermebeltraodev@protonmail.com>
2021-04-30 16:12:31 -0300

    CRUD, attach, detach and sync

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\User;
+use App\Models\Role;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +17,67 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+//create
+Route::get('/user/{user_id}/createrole/{role_name}', function ($user_id, $role_name) {
+
+    $user = User::findOrFail($user_id);
+    $role = new Role(['name'=>$role_name]);
+    $user->roles()->save($role);
+
+});
+
+//read
+Route::get('/user/{user_id}/readroles', function($user_id){
+
+    $user = User::findOrFail($user_id);
+    foreach ($user->roles as $role) {
+        return $role;
+    }
+});
+
+
+//update
+Route::get('/user/{user_id}/updateroles/{old_role_name}/{role_name}', function($user_id,$old_role_name, $new_role_name) {
+
+    $user = User::findOrFail($user_id);
+    if($user->has('roles')){        
+        foreach($user->roles as $role){
+            
+            if($role->name == $old_role_name){
+                $role->name = $new_role_name;
+                $role->save();
+            }
+        }
+    }
+});
+
+//delete
+Route::get('/user/{user_id}/deleterole/{role_name}', function($user_id, $role_name) {
+    $user = User::findOrFail($user_id);
+    $user->roles()->whereName($role_name)->delete();
+});
+
+
+Route::get('user/{user_id}/attachrole/{role_id}', function($user_id, $role_id){
+    $user = User::findOrFail($user_id);
+
+    $user->roles()->attach($role_id);
+});
+
+
+Route::get('/user/{user_id}/detachrole/{role_id}', function($user_id, $role_id){
+
+    $user = User::findOrFail($user_id);
+
+    $user->roles()->detach($role_id);
+});
+
+
+Route::get('/user/{user_id}/syncrole/{role_id}', function($user_id, $role_id){
+    
+    $user = User::findOrFail($user_id);
+    $user->roles()->sync([$role_id]);
+
 });
